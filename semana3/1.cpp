4714d8030828e41c80d37b5d0ac2988da89cd9ca
fbernaldev <fbernal751@gmail.com>
2026-03-16 20:53:31 -0500

    variables

--- semana3/1.cpp
+++ semana3/1.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 using namespace std;
 
+int contador = 0; //Ambito global
+
 int main() {
-
-    
-
+   int contador = 5;
+    contador++;
+    cout<<contador<<endl;
     return 0;
 }
