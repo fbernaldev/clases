b90e5ace9e52e006d17611bafad5fee4d905b037
fbernaldev <fbernal751@gmail.com>
2026-04-03 01:32:01 -0500

    resuelto

--- semana5/6ifelse.cpp
+++ semana5/6ifelse.cpp
@@ -1,11 +1,27 @@
-// Dadas 3 longitudes, comprueba si pueden formar un triángulo. Luego clasifica el tipo de triángulo
-// según sus lados.
+//dada 3 longitudes, comprueba si pueden formar un triangulo, luego clasifica el tipo triangulo
+// segun sus lados
+
 #include <iostream>
 using namespace std;
 
 int main() {
+    int a, b, c;
+    cout<<"Ingrese los 3 lados: ";
+    cin>>a>>b>>c;
+    if (a+b>c && a+c>b && b+c>a){
 
+      if(a==b && a==c){
+        cout<<"EL triangulo es Equilatero";
+
+    }else if (a==b && a!=c){
+    cout<<"EL triangulo es Isosceles";
+
+    }else
+    cout<<"EL triangulo es Escaleno";
+    }else {
+        cout<<"No puedes formar un triangulo "<<endl;
+    }
     
-
+    
     return 0;
 }
