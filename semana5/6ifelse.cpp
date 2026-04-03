cd6dbd17c8fd2965f1c55be43b34492fbfe20182
fbernaldev <fbernal751@gmail.com>
2026-04-03 01:27:06 -0500

    else

---
+++ semana5/6ifelse.cpp
@@ -0,0 +1,11 @@
+// Dadas 3 longitudes, comprueba si pueden formar un triángulo. Luego clasifica el tipo de triángulo
+// según sus lados.
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    
+
+    return 0;
+}
